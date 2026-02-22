7cf8812ac55549d0cb9487b1d27709729b736f73
carlosmi98 <carlosmiguens861@gmail.com>
2026-02-22 23:47:55 +0100

    feat: añadir componentes Boton y GrupoBotones con sus estilos

--- src/componentes/inicio/hero/Hero.js
+++ src/componentes/inicio/hero/Hero.js
@@ -1,16 +1,21 @@
 import imagenPrincipal from "../../../assets/images/inicio/hero/imagenPrincipal.png"
 import { Link } from "react-router-dom"
 import "./Hero.css"
+import GrupoBotones from "../../generales/grupo-botones/GrupoBotones"
+import Boton from "../../generales/botons/Boton"
+
+
 const Hero = () => {
 
     return <section className="principal">
                     <div className="contenido-texto">
                         <h1>Aprende a gestionar tus emociones. Mejora día a día con <span className="marca">FeelifyMe</span>.</h1>
                         <p className="parrafo-h1">Comparte cómo te sientes cada día. No estás solo: tanto en los momentos difíciles como en los buenos, estamos contigo. En <span className="marca">FeelifyMe</span> te ayudamos a comprender tus emociones y te ofrecemos consejos prácticos para aprender a gestionarlas con mayor claridad y equilibrio.</p>
-                        <div className="botones-acceso">
-                            <Link to="#" className="botones-generales">Iniciar sesión</Link>
-                            <Link to="#" className="botones-generales">Crear cuenta</Link>
-                        </div>
+
+                        <GrupoBotones>
+                            <Boton texto = 'Iniciar sesion' to='#' />
+                            <Boton texto = 'Crear cuenta' to='#' />
+                        </GrupoBotones>
                     </div>
                     <div className="contenedor-imagen-principal">
                         <img src={ imagenPrincipal } className="img-principal" />
